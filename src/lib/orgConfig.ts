f414590e82fc6efdb8fcfb285520a5ad2ec0873f
Rene S. <renedsm@gmail.com>
2026-02-23 18:11:12 -0500

    fix: revert celta domain

--- src/lib/orgConfig.ts
+++ src/lib/orgConfig.ts
@@ -125,7 +125,7 @@ export const schoolMap: Record<string, SchoolInfo> = {
             "TC": "https://pr.demo.idsm.xyz"
         },
         "1000": {
-            "MT": "https://mt.celta.interschool.mx",
+            "MT": "https://meta.celta.idsm.xyz",
             "ST": "https://alumno.celta.idsm.xyz",
             "TC": "https://profesor.celta.idsm.xyz"
         },
