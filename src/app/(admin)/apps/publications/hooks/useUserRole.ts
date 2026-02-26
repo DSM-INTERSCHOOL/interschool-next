f6f193f3e08a2d8f8dd09319bde3bcb051b6b031
Rene S. <renedsm@gmail.com>
2026-02-26 17:02:52 -0500

    test: check rol type

--- src/app/(admin)/apps/publications/hooks/useUserRole.ts
+++ src/app/(admin)/apps/publications/hooks/useUserRole.ts
@@ -7,15 +7,19 @@ export const useUserRole = (): UserRole => {
     return useMemo(() => {
         try {
             const { portalName } = getOrgConfig();
+            console.log('portalName', portalName);
 
             if (portalName.toLowerCase().includes('meta') || portalName.toLowerCase().includes('mt')) {
+                console.log("is admin");
                 return 'admin';
             }
 
             if (portalName.toLowerCase().includes('teacher') || portalName.toLowerCase().includes('profesor') || portalName.toLowerCase().includes('pr')) {
+                console.log("is teacher");
                 return 'teacher';
             }
              if (portalName.toLowerCase().includes('student') || portalName.toLowerCase().includes('alumno') || portalName.toLowerCase().includes('al')) {
+                console.log("is alumno");
                 return 'alumno';
             }
 
